57e1af9a4febaf1b0073d34c008c529caecf0603
rusty <rusty.wright@gmail.com>
2021-03-08 21:18:31 -0800

    first files

---
+++ gpio01/main/gpio01.c
@@ -0,0 +1,131 @@
+/*
+ * GPIO Example
+ *
+ * This example code is in the Public Domain (or CC0 licensed, at your
+ * option.)
+ *
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *  CONDITIONS OF ANY KIND, either express or implied.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#include "driver/gpio.h"
+
+/**
+ * Brief: This test code shows how to configure gpio and how to use
+ * gpio interrupt.
+ *
+ * GPIO status:
+ * GPIO02: output
+ * GPIO13: input, pulled up, interrupt from rising edge and falling edge
+ */
+
+#define GPIO_OUTPUT_IO          GPIO_NUM_2
+#define GPIO_OUTPUT_PIN_SEL     (1ULL<<GPIO_OUTPUT_IO)
+
+#define GPIO_INPUT_IO           GPIO_NUM_13
+#define GPIO_INPUT_PIN_SEL      (1ULL<<GPIO_INPUT_IO)
+
+#define ESP_INTR_FLAG_DEFAULT   0
+
+static xQueueHandle gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg) {
+    const uint32_t gpio_num = (uint32_t) arg;
+
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void* arg) {
+    TickType_t previous = 0;
+    int level = 0;
+    uint32_t io_num;
+
+    for (;;) {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%d] intr, val: %d\n",
+                io_num, gpio_get_level(io_num));
+     
+            // button released sets level to 1
+            if (gpio_get_level(io_num) == 0) {
+                TickType_t now = xTaskGetTickCount();
+                TickType_t diff = now - previous;
+                printf("time diff: %u\n", diff);
+
+                if (diff > 30) {
+                    level = !level;
+                    gpio_set_level(GPIO_OUTPUT_IO, level);
+
+                    printf("level: %d\n", level);
+                }
+
+                previous = now;
+            }
+        }
+    }
+}
+
+void app_main(void) {
+    const gpio_config_t io_conf_output = (gpio_config_t) {
+        // disable interrupt
+        .intr_type = GPIO_PIN_INTR_DISABLE,
+        // set as output mode
+        .mode = GPIO_MODE_OUTPUT,
+        // bit mask of the pins that you want to set, e.g. OUTPUT_IO
+        .pin_bit_mask = GPIO_OUTPUT_PIN_SEL,
+        // disable pull-down mode
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        // disable pull-up mode
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+    };
+
+    // configure GPIO with the given settings
+    gpio_config(&io_conf_output);
+
+    const gpio_config_t io_conf_input = (gpio_config_t) {
+        // interrupt of rising edge
+        .intr_type = GPIO_PIN_INTR_NEGEDGE,
+        // set as input mode    
+        .mode = GPIO_MODE_INPUT,
+        // bit mask of the pins, use INPUT_IO here
+        .pin_bit_mask = GPIO_INPUT_PIN_SEL,
+        // disable pull-down mode
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        // enable pull-up mode
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+    };
+
+    // configure GPIO with the given settings
+    gpio_config(&io_conf_input);
+
+    // change gpio intrrupt type for one pin
+    gpio_set_intr_type(GPIO_INPUT_IO, GPIO_INTR_NEGEDGE);
+
+    // create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+
+    // start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example",
+                2048, NULL, 10, NULL);
+
+    // install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+
+    // hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO, gpio_isr_handler,
+                         (void*) GPIO_INPUT_IO);
+
+    gpio_set_level(GPIO_OUTPUT_IO, 0);
+
+    for (int cnt = 0; /**/; cnt++) {
+        printf("cnt: %d\n", cnt);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+}
---
+++ ledc01/main/ledc_example_main.c
@@ -0,0 +1,266 @@
+/*
+ * LEDC (LED Controller) fade example
+ *
+ * This example code is in the Public Domain (or CC0 licensed, at
+ * your option.)
+ *
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ */
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+
+/*
+ * About this example
+ *
+ * 1. Start with initializing LEDC module:
+ *    a. Set the timer of LEDC first, this determines the frequency
+ *       and resolution of PWM.
+ *    b. Then set the LEDC channel you want to use,
+ *       and bind with one of the timers.
+ *
+ * 2. You need first to install a default fade function,
+ *    then you can use fade APIs.
+ *
+ * 3. You can also set a target duty directly without fading.
+ *
+ * 4. This example uses GPIO18/19/4/5 as LEDC output,
+ *    and it will change the duty repeatedly.
+ *
+ * 5. GPIO18/19 are from high speed channel group.
+ *    GPIO4/5 are from low speed channel group.
+ */
+#define LEDC_HS_TIMER          LEDC_TIMER_0
+#define LEDC_HS_MODE           LEDC_HIGH_SPEED_MODE
+
+#define LEDC_HS_CH0_GPIO       18
+#define LEDC_HS_CH0_CHANNEL    LEDC_CHANNEL_0
+#define LEDC_HS_CH1_GPIO       19
+#define LEDC_HS_CH1_CHANNEL    LEDC_CHANNEL_1
+
+#define LEDC_LS_TIMER          LEDC_TIMER_1
+#define LEDC_LS_MODE           LEDC_LOW_SPEED_MODE
+
+#define LEDC_LS_CH2_GPIO       4
+#define LEDC_LS_CH2_CHANNEL    LEDC_CHANNEL_2
+#define LEDC_LS_CH3_GPIO       5
+#define LEDC_LS_CH3_CHANNEL    LEDC_CHANNEL_3
+
+#define LEDC_TEST_CH_NUM       (4)
+
+/* final brightness */
+//# define LEDC_TEST_DUTY         (2000)
+//# define DUTY_RESOLUTION	LEDC_TIMER_12_BIT
+
+# define LEDC_TEST_DUTY         (1000)
+# define DUTY_RESOLUTION	LEDC_TIMER_13_BIT
+
+#define LEDC_TEST_FADE_TIME    (6000)
+//#define LEDC_TEST_FADE_TIME    (3000)
+
+# define DELAY			(LEDC_TEST_FADE_TIME + 3000)
+# define DELAY_ERROR		10000
+
+/*
+ * resolution of pulses; bigger = more finely divided.
+ * around 30 and below produces flickering.
+ */
+# define PWM_FREQ_HZ		40
+//# define PWM_FREQ_HZ		5000
+
+/*
+ * Prepare and set configuration of timers that will be used by
+ * LED Controller
+ */
+void config_timers() {
+    // Set configuration of timer0 for low speed channels
+    const ledc_timer_config_t ledc_timer_ls = {
+        // resolution of PWM duty
+	.duty_resolution = DUTY_RESOLUTION,
+	// frequency of PWM signal
+        .freq_hz = PWM_FREQ_HZ,
+	// timer mode
+        .speed_mode = LEDC_LS_MODE,
+	// timer index
+        .timer_num = LEDC_LS_TIMER,
+	// Auto select the source clock
+        .clk_cfg = LEDC_AUTO_CLK
+    };
+    
+    if (ledc_timer_config(&ledc_timer_ls) != ESP_OK) {
+	printf("low speed timer config failed\n");
+	vTaskDelay(DELAY_ERROR / portTICK_PERIOD_MS);
+    }
+
+    // Set configuration of timer0 for high speed channels
+    const ledc_timer_config_t ledc_timer_hs = {
+        // resolution of PWM duty
+        .duty_resolution = DUTY_RESOLUTION,
+	// frequency of PWM signal
+        .freq_hz = PWM_FREQ_HZ,
+	// timer mode
+        .speed_mode = LEDC_HS_MODE,
+	// timer index
+        .timer_num = LEDC_HS_TIMER,
+	// Auto select the source clock
+        .clk_cfg = LEDC_AUTO_CLK
+    };
+
+    if (ledc_timer_config(&ledc_timer_hs) != ESP_OK) {
+	printf("high speed timer config failed\n");
+	vTaskDelay(DELAY_ERROR / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main(void) {
+    int ch;
+
+    config_timers();
+    
+    /*
+     * Prepare individual configuration
+     * for each channel of LED Controller
+     * by selecting:
+     * - controller's channel number
+     * - output duty cycle, set initially to 0
+     * - GPIO number where LED is connected to
+     * - speed mode, either high or low
+     * - timer servicing selected channel
+     *   Note: if different channels use one timer,
+     *         then frequency and bit_num of these channels
+     *         will be the same
+     */
+    ledc_channel_config_t ledc_channel[LEDC_TEST_CH_NUM] = {
+        {
+	 .channel    = LEDC_HS_CH0_CHANNEL,
+	 .duty       = 0,
+	 .gpio_num   = LEDC_HS_CH0_GPIO,
+	 .speed_mode = LEDC_HS_MODE,
+	 .hpoint     = 0,
+	 .timer_sel  = LEDC_HS_TIMER
+        },
+        {
+	 .channel    = LEDC_HS_CH1_CHANNEL,
+	 .duty       = 0,
+	 .gpio_num   = LEDC_HS_CH1_GPIO,
+	 .speed_mode = LEDC_HS_MODE,
+	 .hpoint     = 0,
+	 .timer_sel  = LEDC_HS_TIMER
+        },
+        {
+	 .channel    = LEDC_LS_CH2_CHANNEL,
+	 .duty       = 0,
+	 .gpio_num   = LEDC_LS_CH2_GPIO,
+	 .speed_mode = LEDC_LS_MODE,
+	 .hpoint     = 0,
+	 .timer_sel  = LEDC_LS_TIMER
+        },
+        {
+	 .channel    = LEDC_LS_CH3_CHANNEL,
+	 .duty       = 0,
+	 .gpio_num   = LEDC_LS_CH3_GPIO,
+	 .speed_mode = LEDC_LS_MODE,
+	 .hpoint     = 0,
+	 .timer_sel  = LEDC_LS_TIMER
+        },
+    };
+
+    // Set LED Controller with previously prepared configuration
+    for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
+        if (ledc_channel_config(&ledc_channel[ch]) != ESP_OK) {
+	    printf("ledc_channel_config failed, ch: %d\n", ch);
+	    vTaskDelay(DELAY_ERROR / portTICK_PERIOD_MS);
+	}
+    }
+
+    // Initialize fade service.
+    ledc_fade_func_install(0);
+
+    for (;;) {
+        printf("1. LEDC fade up to duty = %d\n", LEDC_TEST_DUTY);
+
+        for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
+            if (ledc_set_fade_with_time(ledc_channel[ch].speed_mode,
+                    ledc_channel[ch].channel,
+                    LEDC_TEST_DUTY,
+                    LEDC_TEST_FADE_TIME) != ESP_OK) {
+		printf("ledc_channel_config failed 1, ch: %d\n", ch);
+		vTaskDelay(DELAY_ERROR / portTICK_PERIOD_MS);
+	    }
+	    
+            if (ledc_fade_start(ledc_channel[ch].speed_mode,
+                    ledc_channel[ch].channel,
+		    LEDC_FADE_NO_WAIT) != ESP_OK) {
+		printf("ledc_channel_config failed 2, ch: %d\n", ch);
+		vTaskDelay(DELAY_ERROR / portTICK_PERIOD_MS);
+	    }
+        }
+
+        vTaskDelay(DELAY / portTICK_PERIOD_MS);
+
+        printf("2. LEDC fade down to duty = 0\n");
+
+        for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
+            if (ledc_set_fade_with_time(ledc_channel[ch].speed_mode,
+	            ledc_channel[ch].channel,
+		    0,
+		    LEDC_TEST_FADE_TIME) != ESP_OK) {
+		printf("ledc_channel_config failed, ch: %d\n", ch);
+		vTaskDelay(DELAY_ERROR / portTICK_PERIOD_MS);
+	    }
+	    
+            if (ledc_fade_start(ledc_channel[ch].speed_mode,
+                    ledc_channel[ch].channel,
+                    LEDC_FADE_NO_WAIT) != ESP_OK) {
+		printf("ledc_channel_config failed, ch: %d\n", ch);
+		vTaskDelay(DELAY_ERROR / portTICK_PERIOD_MS);
+	    }
+        }
+
+        vTaskDelay(DELAY /  portTICK_PERIOD_MS);
+
+# ifdef notdef
+        printf("3. LEDC set duty = %d without fade\n", LEDC_TEST_DUTY);
+
+        for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
+            if (ledc_set_duty(ledc_channel[ch].speed_mode,
+			      ledc_channel[ch].channel,
+			      LEDC_TEST_DUTY) != ESP_OK) {
+		printf("ledc_channel_config failed, ch: %d\n", ch);
+		vTaskDelay(DELAY_ERROR / portTICK_PERIOD_MS);
+	    }
+	    
+            if (ledc_update_duty(ledc_channel[ch].speed_mode,
+				 ledc_channel[ch].channel) != ESP_OK) {
+		printf("ledc_channel_config failed, ch: %d\n", ch);
+		vTaskDelay(DELAY_ERROR / portTICK_PERIOD_MS);
+	    }
+        }
+
+        vTaskDelay(DELAY / portTICK_PERIOD_MS);
+
+        printf("4. LEDC set duty = 0 without fade\n");
+
+        for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
+            if (ledc_set_duty(ledc_channel[ch].speed_mode,
+			      ledc_channel[ch].channel,
+			      0) != ESP_OK) {
+		printf("ledc_channel_config failed, ch: %d\n", ch);
+		vTaskDelay(DELAY_ERROR / portTICK_PERIOD_MS);
+	    }
+	    
+            if (ledc_update_duty(ledc_channel[ch].speed_mode,
+				 ledc_channel[ch].channel) != ESP_OK) {
+		printf("ledc_channel_config failed, ch: %d\n", ch);
+		vTaskDelay(DELAY_ERROR / portTICK_PERIOD_MS);
+	    }
+        }
+
+        vTaskDelay(DELAY / portTICK_PERIOD_MS);
+# endif
+    }
+}
