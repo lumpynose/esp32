1b0172cd6f765db1a042ed9fa6c47d70f800c026
rusty <rusty.wright@gmail.com>
2021-03-21 21:59:17 -0700

    update

--- python/tinypico-main.py
+++ python/tinypico-main.py
@@ -1,11 +1,11 @@
-from machine import SPI, Pin
+from machine import Pin, SoftSPI
 import tinypico as TinyPICO
 from dotstar import DotStar
 import time, random, micropython, gc
 
 # Configure SPI for controlling the DotStar
 # Internally we are using software SPI for this as the pins being used are not hardware SPI pins
-spi = SPI(sck=Pin( TinyPICO.DOTSTAR_CLK ), mosi=Pin( TinyPICO.DOTSTAR_DATA ), miso=Pin( TinyPICO.SPI_MISO) ) 
+spi = SoftSPI(sck=Pin( TinyPICO.DOTSTAR_CLK ), mosi=Pin( TinyPICO.DOTSTAR_DATA ), miso=Pin( TinyPICO.SPI_MISO) ) 
 # Create a DotStar instance
 dotstar = DotStar(spi, 1, brightness = 0.5 ) # Just one DotStar, half brightness
 # Turn on the power to the DotStar
